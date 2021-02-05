bb6f22e2168d50940b500c48aa18e87e7d699a65
pritam-rgb <pritamchatterjee945@gmail.com>
2021-02-05 20:26:46 +0530

    Image can be rendered Succesfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    
+    let currentImageIndex=0;
+    let customImageIndex=["_200","_400",""];
+    let add="_100";
+    this.handleError= ()=>{
+      console.log("Called");
+      add=customImageIndex[currentImageIndex];
+      console.log(add);
+      currentImageIndex++;
+      
+    }
+    let image_path=`https://cdn.quicksell.co/-KpFi1XEDgH1mY5k1fSo/products${add}/-M7q3olCZCehB9AGbn2y.jpg`
+    
+    return (
+      <div className="container">
+        <img src={image_path} 
+        alt="" 
+        onError={()=>this.handleError()} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
